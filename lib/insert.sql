16e14dd99d35aea8f552ee79f15dcca8dddd4d6f
Moomal <moomalmsoomro@gmail.com>
2021-02-04 23:26:20 -0500

    commit tree 95b963b76447895277f77f73f162731b69a7ab57

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 50, "M", "light brown", "goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 24, "M", "brown", "laid back", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 23, "F", "black and white", "emotional", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 46, "M", "dark brown", "energetic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 150, "F", "dark brown", "cool", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 24, "M", "light brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 95, "M", "yellow", "hungry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 22, "M", "white", "rug", 0);
